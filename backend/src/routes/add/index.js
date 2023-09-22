dc88b27b83b158c510d65fe51ee2506beab34804
Majd Al Mnayer <majd.mounayer@gmail.com>
2023-09-22 17:53:50 -0400

    Added S3

--- backend/src/routes/add/index.js
+++ backend/src/routes/add/index.js
@@ -1,10 +1,13 @@
 const { getKMSKey } = require("../../encryption/index");
+const { writeData } = require("../../db/index");
 
 module.exports = async function (req, res) {
   let username = req.query.username;
 
   try {
     let keyid = await getKMSKey(username);
+
+    await writeData(username, keyid);
 
     //res.status(200);
   } catch (err) {
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ const Express = require("express");
 const dotenv = require("dotenv");
 const encrypt = require("../middlewares/encrypt");
 const { authenticate } = require("../authentication/index");
+const add = require("./add/index");
 
 dotenv.config();
 
@@ -10,5 +11,7 @@ const router = Express.Router();
 router.get("/", authenticate(), (req, res) => {
   res.status(200).send(`Health is good!`);
 });
+
+router.get("/add", authenticate(), add);
 
 module.exports.router = router;
