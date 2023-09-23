bd3893b6d677da6b4865194d4e22722a0117edd5
mrahman141 <mrahman141@myseneca.ca>
2023-09-23 00:55:13 -0400

    good looking form but a big issue so DONT PUSH

--- frontend/diary-front-end/src/components/form/index.js
+++ frontend/diary-front-end/src/components/form/index.js
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useRef } from "react";
 
+
+
 export default function Form() {
   // retrieves the following properties and member functions from useForm
   const { register, handleSubmit } = useForm({
@@ -53,7 +55,7 @@ export default function Form() {
     // Set isText only if it's a text or application/json
     setIsText(
       event.target.value.startsWith("text/") ||
-        event.target.value === "application/json"
+      event.target.value === "application/json"
     );
 
     // Clear the file input if it exists
@@ -166,28 +168,33 @@ export default function Form() {
     }
   }
 
-  return (
-    <div className="centered">
-      <h1>Secure Your Information</h1>
-      <h2>Information Type</h2>
-      <div>
-        <select value={selectedOption} onChange={handleChange}>
-          <option value="text/plain">Text</option>
-          <option value="application/json">application/json</option>
-          <option value="image/jpeg">Image</option>
 
-          <option value="application/octet-stream">Any File</option>
-        </select>
-      </div>
-      <form onSubmit={handleSubmit(whatToDo)}>
-        <h2>Title: </h2>
-        <input
-          type="text"
-          {...register("title")}
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        ></input>
-        {/* <h2>Data: </h2>
+
+
+  return (
+    <>
+      <div className="centered w-full h-screen bg-blue-800 text-white">
+        <h1 className="text-2xl text-center">Secure Your Information (feels a bit off centered maybe becuz of scroll bar)</h1>
+
+        <div className="px-5">
+          <label for="countries" class="block mb-2 text-sm font-medium text-white">Information Type</label>
+          <select value={selectedOption} onChange={handleChange} id="countries" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-72 p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="text/plain">Text</option>
+            <option value="application/json">application/json</option>
+            <option value="image/jpeg">Image</option>
+            <option value="application/octet-stream">Files</option>
+          </select>
+        </div>
+
+        <form onSubmit={handleSubmit(whatToDo)}>
+          <br />
+          <div className="px-5">
+            <div class="mb-6">
+              <label for="default-input" class="block mb-2 text-sm font-medium text-white">Title</label>
+              <input type="text" id="default-input" {...register("title")} onChange={(e) => setTitle(e.target.value)} value={title} class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-72 p-2.5" />
+            </div>
+          </div>
+          {/* <h2>Data: </h2>
         {isText ? (
           <input type="text" {...register("payload")}></input>
         ) : (
@@ -198,34 +205,52 @@ export default function Form() {
             accept={selectedOption}
           ></input>
         )} */}
-        <h2>Data: </h2>
-        {isText ? (
-          <input type="text" {...register("payload")} />
-        ) : isImage ? (
-          <input
-            type="file"
-            name="file"
-            ref={fileInput}
-            accept=".gif, .jpeg, .jpg, .png, .webp"
-            onChange={handleFileChange}
-          />
-        ) : (
-          <input type="file" name="file" ref={fileInput} />
-        )}
-        <br /> <br />
-        <button type="submit">Submit</button> &nbsp;
-        <button type="button" onClick={goHome}>
-          Home
-        </button>
-      </form>
-      <br></br>
+          <label for="default-input" class="block mb-2 text-sm font-medium text-white px-5">Data</label>
+          {isText ? (
+            <><div class="bg-white border border-slate-200 grid grid-cols-6 gap-2 rounded-xl p-2 text-sm m-5">
+              <h1 class="text-center text-xl italic col-span-6 text-black">Write Something</h1>
+              <div class="flex justify-center col-span-6 px-5 py-3">
+                <textarea type="text" {...register("payload")} placeholder="Whatever your heart desires..." class="bg-slate-100 text-slate-600 h-28 w-screen placeholder:text-slate-600 placeholder:opacity-50 border border-slate-200 resize-none outline-none rounded-lg p-2 duration-300 focus:border-slate-600"></textarea>
+              </div>
+            </div></>
+            // <input type="text" {...register("payload")} />
+          ) : isImage ? (
+            <div className="px-5">
+              {/* <div class="flex items-center justify-center w-1/2 px-5">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                  <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                    </svg>
+                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">.gif, .jpeg, .jpg, .png, .webp</p>
+                  </div>
+                  <input id="dropzone-file" type="file" class="hidden" name="file" ref={fileInput} accept=".gif, .jpeg, .jpg, .png, .webp" onChange={handleFileChange} />
+                </label>
+              </div> */}
+              <input type="file" name="file" ref={fileInput} accept=".gif, .jpeg, .jpg, .png, .webp" onChange={handleFileChange} class="block w-90 text-lg text-black border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-black focus:outline-none dark:border-gray-600 dark:placeholder-gray-400" />
+              <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">PNG, JPG ,GIF or WEBP</p>
+            </div>
+          ) : (
+            <div>
+              <input type="file" name="file" ref={fileInput} />
+            </div>
+          )}
+          <br /> <br />
+          <button type="submit">Submit</button> &nbsp;
+          <button type="button" onClick={goHome}>
+            Home
+          </button>
+        </form>
+        <br></br>
 
-      <br></br>
-      {data && (
-        <>
-          <h2>Server Response</h2>
-        </>
-      )}
-    </div>
+        <br></br>
+        {data && (
+          <>
+            <h2>Server Response</h2>
+          </>
+        )}
+      </div>
+    </>
   );
 }
