26101d6ee55d6337fba987785763a708f4bb5f73
Majd Al Mnayer <majd.mounayer@gmail.com>
2023-09-22 21:38:06 -0400

    Added header and footer

---
+++ frontend/diary-front-end/src/components/header/index.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <footer className="bg-teal-800 py-6">
+      <div className="container mx-auto text-center text-white font-bold text-xl">
+        LockIt
+      </div>
+    </footer>
+  );
+}
+
+export default Header;
