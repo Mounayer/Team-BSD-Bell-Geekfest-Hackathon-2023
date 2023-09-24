18b9a882ad314fcad5a9c290955e5b941cd9edfa
Tatiana <86887485+Amoraa@users.noreply.github.com>
2023-09-23 23:26:23 -0400

    styling 4 pages

--- frontend/diary-front-end/app/about-us/page.js
+++ frontend/diary-front-end/app/about-us/page.js
@@ -1,31 +1,27 @@
-import React from 'react'
-import TextParagraph from '@/src/components/text/textParagraph'
-import Header from '@/src/components/text/textHeader'
+import React from 'react';
+import Card from '@/src/components/text/card';
 
+export default function Page() {
+  return (  
+    <div className="flex justify-center m-10">
+      {/* Right Column */}
+      <div className="w-80 mr-8">
+        <Card title="About Us" text="At Lock-it, we are passionate about providing you with a safe and reliable solution for storing and managing your data. Our commitment to security, integrity, and innovation drives everything we do." />
+      </div>
 
-export default function page() {
-  return (
-    <>  <div className="flex m-10">
-   
-
-    {/* Right Column */}
-    <div className="w-40%">
-    <Header text ="AAAA" />
-
-    </div> 
-    {/* Left Column */}
-    <div className="w-60%">
-      <img
-        src="logo-name-nowhite.png"
-        alt="Image 1"
-        className="w-full h-auto"
-      />
-      <img
-        src="logo-name-nowhite.png"
-        alt="Image 2"
-        className="w-full h-auto"
-      />
+      {/* Left Column */}
+      <div className="w-80">
+        <img
+          src="logo-name-nowhite.png"
+          alt="Image 1"
+          className="w-full h-auto" // Keep the second image as it is
+        />
+        <img
+          src="logo-name-nowhite.png"
+          alt="Image 2"
+          className="w-2/3 h-auto" // Make the first image bigger (adjust width as needed)
+        />
+      </div>
     </div>
-  </div></>
-  )
+  );
 }
--- frontend/diary-front-end/src/components/text/textHeader.js
+++ frontend/diary-front-end/src/components/text/textHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ text }) => {
   return (
-    <header className="py-4 text-white font-almarai font-semibold text-2xl text-center">
+    <header className="py-4 text-black font-almarai font-semibold text-2xl text-center">
       {text}
     </header>
   );
